2f39e79b98ab1d898a9d5e4b828b15bb13af7fa4
fandi-adhitya <fandiadhitya96@gmail.com>
2023-07-26 22:37:59 +0700

    Initial

---
+++ app/src/main/java/com/fandiadhitya/ageinminute/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fandiadhitya.ageinminute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
