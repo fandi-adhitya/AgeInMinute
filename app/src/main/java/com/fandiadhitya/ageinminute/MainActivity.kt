11fbc62bbf9dc203d729c74d9a5606028c3cd92a
fandi-adhitya <fandiadhitya96@gmail.com>
2023-09-30 16:55:02 +0700

    chore: calculate date in minutes

--- app/src/main/java/com/fandiadhitya/ageinminute/MainActivity.kt
+++ app/src/main/java/com/fandiadhitya/ageinminute/MainActivity.kt
@@ -3,14 +3,20 @@ package com.fandiadhitya.ageinminute
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private var tvSelectedDate: TextView? = null
+    private var tvAgeInMinutes: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,33 +25,65 @@ class MainActivity : AppCompatActivity() {
 
         val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
         btnDatePicker.setOnClickListener {
             clickDatePicker()
         }
     }
 
-    fun clickDatePicker() {
+    private fun clickDatePicker() {
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
+        val datePickerDialog = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+            DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDayOfMonth ->
                 Toast.makeText(
                     this,
                     "Year was $selectedYear, month was ${selectedMonth + 1}, day of month was $selectedDayOfMonth",
                     Toast.LENGTH_LONG
                 ).show()
 
-                val selectedDate = "$selectedDayOfMonth/$selectedMonth/$selectedYear"
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+
                 tvSelectedDate?.setText(selectedDate)
+
+                val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = simpleDateFormat.parse(selectedDate)
+
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / 60000
+
+                    val currentDate =
+                        simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))
+
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / 60000
+
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                        tvAgeInMinutes?.setText(formatWithThousandSeparator(differenceInMinutes))
+
+                    }
+                }
             },
             year,
             month,
             day
-        ).show()
+        )
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        datePickerDialog.show()
+    }
+
+    private fun formatWithThousandSeparator(number: Long): String {
+        val formatter = NumberFormat.getNumberInstance(Locale.US)
+        if (formatter is DecimalFormat) {
+            formatter.applyPattern("#,###")
+        }
+        return formatter.format(number)
     }
 }
